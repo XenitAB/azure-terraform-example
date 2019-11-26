f55aaac0b753184452f50ac2b3c0e73daf57e69b
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-11-26 16:29:15 +0100

    Fix subnets

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -1,6 +1,12 @@
 resource "azurerm_network_security_group" "nsg" {
-  count = length(var.vnetConfig.subnets)
+  count               = length(var.vnetConfig.subnets)
   name                = "nsg-${var.environmentShort}-${var.locationShort}-${var.commonName}-${var.vnetConfig.subnets[count.index].name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsgAssociations" {
+  count                     = length(var.vnetConfig.subnets)
+  subnet_id                 = azurerm_subnet.subnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg[count.index].id
 }
--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -10,25 +10,4 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = var.vnetConfig.addressSpace
-
-  dynamic "subnet" {
-    for_each = [for s in var.vnetConfig.subnets: {
-      subnetName      = "sn-${var.environmentShort}-${var.locationShort}-${var.commonName}-${s.name}"
-      prefix          = s.cidr
-      nsgName         = "nsg-${var.environmentShort}-${var.locationShort}-${var.commonName}-${s.name}"
-    }]
-
-    content {
-      name           = subnet.value.subnetName
-      address_prefix = subnet.value.prefix
-      security_group = subnet.value.nsgName
-    }
-  }
-}
-
-resource "azurerm_subnet" "gwSubnet" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = var.gatewaySubnet
 }
