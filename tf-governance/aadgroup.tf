51cbc138783deb27eb6d6cfe5173c07cb5f28e8d
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-11-27 13:03:09 +0100

    Add owner, contributor and reader groups

--- tf-governance/aadgroup.tf
+++ tf-governance/aadgroup.tf
@@ -1,12 +1,38 @@
-# data "azurerm_subscription" "azureSubscription" {}
-
-resource "azuread_group" "aadgroup" {
+# AAD Group for Owners
+resource "azuread_group" "aadGroupOwner" {
     count     = length(var.rgConfig)
-    name    = "aad-sec-u-${var.environmentShort}-${var.rgConfig[count.index].commonName}"
+    name    = "aad-sec-u-${var.environmentShort}-${var.rgConfig[count.index].commonName}-owner"
 }
 
-# resource "azurerm_role_assignment" "example" {
-#   scope                = "${data.azurerm_subscription.primary.id}"
-#   role_definition_name = "Reader"
-#   principal_id         = "${data.azurerm_client_config.example.service_principal_object_id}"
-# }
+resource "azurerm_role_assignment" "roleAssignmentOwner" {
+    count                = length(var.rgConfig)
+    scope                = azurerm_resource_group.rg[count.index].id
+    role_definition_name = "Owner"
+    principal_id         = azuread_group.aadGroupOwner[count.index].id
+}
+
+# AAD Group for Contributors
+resource "azuread_group" "aadGroupContributor" {
+    count     = length(var.rgConfig)
+    name    = "aad-sec-u-${var.environmentShort}-${var.rgConfig[count.index].commonName}-contributor"
+}
+
+resource "azurerm_role_assignment" "roleAssignmentContributor" {
+    count                = length(var.rgConfig)
+    scope                = azurerm_resource_group.rg[count.index].id
+    role_definition_name = "Contributor"
+    principal_id         = azuread_group.aadGroupContributor[count.index].id
+}
+
+# AAD Group for Readers
+resource "azuread_group" "aadGroupReader" {
+    count     = length(var.rgConfig)
+    name    = "aad-sec-u-${var.environmentShort}-${var.rgConfig[count.index].commonName}-reader"
+}
+
+resource "azurerm_role_assignment" "roleAssignmentReader" {
+    count                = length(var.rgConfig)
+    scope                = azurerm_resource_group.rg[count.index].id
+    role_definition_name = "Reader"
+    principal_id         = azuread_group.aadGroupReader[count.index].id
+}
