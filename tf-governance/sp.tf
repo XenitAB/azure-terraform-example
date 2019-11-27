2493d3014610b1660b9a6de7c78b242d613fc93c
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-11-27 19:49:30 +0100

    Add service principals

---
+++ tf-governance/sp.tf
@@ -0,0 +1,16 @@
+resource "azuread_application" "aadApp" {
+    for_each                   = {for rg in var.rgConfig: rg.commonName => rg}
+    name                       = "sp-${var.environmentShort}-${each.value.commonName}-contributor"
+}
+
+resource "azuread_service_principal" "aadSp" {
+    for_each                     = {for rg in var.rgConfig: rg.commonName => rg}
+    application_id               = azuread_application.aadApp[each.key].application_id
+}
+
+resource "azurerm_role_assignment" "roleAssignmentSp" {
+    for_each             = {for rg in var.rgConfig: rg.commonName => rg}
+    scope                = azurerm_resource_group.rg[each.key].id
+    role_definition_name = "Contributor"
+    principal_id         = azuread_service_principal.aadSp[each.key].object_id
+}
