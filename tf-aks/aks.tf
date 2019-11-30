bdfb41451c586f36c0a9939577313d679bfb1a16
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-11-30 20:37:06 +0100

    Configure k8s version

--- tf-aks/aks.tf
+++ tf-aks/aks.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   dns_prefix          = "aks-${var.environmentShort}-${var.locationShort}-${var.commonName}"
+  kubernetes_version  = "1.14.8"
 
   default_node_pool {
     name       = "default"
