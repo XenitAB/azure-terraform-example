576d4755c4a6a70244abfecd938dd49d835d9487
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-12-04 14:54:42 +0100

    Initial vm deploy

---
+++ tf-vm/vm.tf
@@ -0,0 +1,57 @@
+locals {
+  vmBaseName = "vm-${var.environmentShort}-${var.locationShort}-${var.commonName}"
+}
+
+data "azurerm_key_vault_secret" "vmPassword" {
+  name         = local.vmBaseName
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = var.vmConfig.count
+  name                = "nic-${local.vmBaseName}-${format("%02s", count.index + 1)}-01"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig-01"
+    subnet_id                     = data.azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  count               = var.vmConfig.count
+  name                = "${local.vmBaseName}-${format("%02s", count.index + 1)}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  vm_size             = var.vmConfig.size
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  storage_image_reference {
+    publisher = var.vmConfig.storageImageReference.publisher
+    offer     = var.vmConfig.storageImageReference.offer
+    sku       = var.vmConfig.storageImageReference.sku
+    version   = var.vmConfig.storageImageReference.version
+  }
+
+  storage_os_disk {
+    name              = "osdisk-${local.vmBaseName}-${format("%02s", count.index + 1)}"
+    caching           = var.vmConfig.storageOsDisk.caching
+    create_option     = var.vmConfig.storageOsDisk.create_option
+    managed_disk_type = var.vmConfig.storageOsDisk.managed_disk_type
+  }
+
+  os_profile {
+    computer_name  = "${local.vmBaseName}-${format("%02s", count.index + 1)}"
+    admin_username = var.vmConfig.username
+    admin_password = data.azurerm_key_vault_secret.vmPassword.value
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
