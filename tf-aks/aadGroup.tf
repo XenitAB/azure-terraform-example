415f251126a4fe7edc319095dc909dd38f21d310
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-12-04 09:57:50 +0100

    Update group names for aks

--- tf-aks/aadGroup.tf
+++ tf-aks/aadGroup.tf
@@ -1,17 +1,17 @@
 resource "azuread_group" "aadGroupView" {
   for_each = { for ns in var.k8sNamespaces : ns.name => ns }
-  name     = "aad-k8s-u-${var.environmentShort}-${each.value.name}-view"
+  name     = "aks-${var.environmentShort}-${each.value.name}-view"
 }
 
 resource "azuread_group" "aadGroupEdit" {
   for_each = { for ns in var.k8sNamespaces : ns.name => ns }
-  name     = "aad-k8s-u-${var.environmentShort}-${each.value.name}-edit"
+  name     = "aks-${var.environmentShort}-${each.value.name}-edit"
 }
 
 resource "azuread_group" "aadGroupClusterAdmin" {
-  name = "aad-k8s-u-${var.environmentShort}-clusteradmin"
+  name = "aks-${var.environmentShort}-clusteradmin"
 }
 
 resource "azuread_group" "aadGroupClusterView" {
-  name = "aad-k8s-u-${var.environmentShort}-clusterview"
+  name = "aks-${var.environmentShort}-clusterview"
 }
