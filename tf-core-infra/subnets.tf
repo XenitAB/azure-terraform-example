047728736e935f42bfaeca034116c8909e079237
Simon Gottschlag <simon.gottschlag@xenit.se>
2019-12-09 09:40:10 +0100

    Add deprecated nsg reference

--- tf-core-infra/subnets.tf
+++ tf-core-infra/subnets.tf
@@ -1,9 +1,10 @@
 resource "azurerm_subnet" "subnet" {
-  for_each                    = {for subnet in var.vnetConfig.subnets: subnet.name => subnet}
-  name                        = "sn-${var.environmentShort}-${var.locationShort}-${var.commonName}-${each.value.name}"
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  virtual_network_name        = azurerm_virtual_network.vnet.name
-  address_prefix              = each.value.cidr
+  for_each                  = { for subnet in var.vnetConfig.subnets : subnet.name => subnet }
+  name                      = "sn-${var.environmentShort}-${var.locationShort}-${var.commonName}-${each.value.name}"
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  address_prefix            = each.value.cidr
+  network_security_group_id = azurerm_network_security_group.nsg[each.value.name].id
 }
 
 resource "azurerm_subnet" "gwSubnet" {
