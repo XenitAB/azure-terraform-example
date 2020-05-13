344a1ab4b4dfa3c70df76c8d79699ee7915d23ef
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-05-13 23:22:48 +0200

    Move repo

--- tf-azpipelines/variables/qa.tfvars
+++
@@ -1,5 +0,0 @@
-environmentShort = "qa"
-envVmConfig = {
-  count = 2
-  size  = "Standard_B4ms"
-}
--- tf-vm/kv.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_key_vault" "kv" {
-  name                = "kv-${var.environmentShort}-${var.locationShort}-${var.coreConfig.commonName}"
-  resource_group_name = "rg-${var.environmentShort}-${var.locationShort}-${var.coreConfig.commonName}"
-}
